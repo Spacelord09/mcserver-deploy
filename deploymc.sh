a808fc99ddaeaf4538b0f0b57281a91817d37466
Spacelord <spacelord09@users.noreply.github.com>
2020-04-08 20:39:58 +0200

    Remove git reset

--- deploymc.sh
+++ deploymc.sh
@@ -206,7 +206,6 @@ cat >> /var/www/hooks/update-${server_name}.log
 
 cd $home_dir
 
-sudo -u $user_name git reset --hard 2>&1
 sudo -u $user_name git pull origin master --recurse-submodules 2>&1
 sudo -u $user_name git checkout master 2>&1
 
